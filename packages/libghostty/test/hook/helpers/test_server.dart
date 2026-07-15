f8f756e419f2097ba5d80e2f777e7b4f8d7ff0d6
Adon Metcalfe <adonm@fastmail.fm>
2026-07-15 20:28:10 +0800

    fix(libghostty): serialize source cache population

--- packages/libghostty/test/hook/helpers/test_server.dart
+++ packages/libghostty/test/hook/helpers/test_server.dart
@@ -6,16 +6,27 @@ import 'package:shelf_static/shelf_static.dart';
 class TestServer {
   final Uri baseUrl;
   final HttpServer _server;
+  final List<void> _requests;
   Future<void>? _closeFuture;
 
-  TestServer._(this._server, this.baseUrl);
+  TestServer._(this._server, this.baseUrl, this._requests);
+
+  int get requestCount => _requests.length;
 
   Future<void> close() => _closeFuture ??= _server.close();
 
   static Future<TestServer> start(Directory directory) async {
-    final handler = createStaticHandler(directory.path);
-    final server = await io.serve(handler, 'localhost', 0);
+    final staticHandler = createStaticHandler(directory.path);
+    final requests = <void>[];
+    final server = await io.serve(
+      (request) {
+        requests.add(null);
+        return staticHandler(request);
+      },
+      'localhost',
+      0,
+    );
     final baseUrl = Uri.parse('http://localhost:${server.port}');
-    return TestServer._(server, baseUrl);
+    return TestServer._(server, baseUrl, requests);
   }
 }
